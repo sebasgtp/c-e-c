52d06cc0d3e7b4de80ed631b524a84b911d66b9b
sebasgtp <sgutierrezpi@unal.edu.co>
2025-05-14 12:16:40 -0500

    Se crea una función que permite la cantidad de entradas, localidad y peguntar si desea otra localidad

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ const segundaLocalidadSelect = document.getElementById('segundaLocalidad');
 const resultado = document.getElementById('resultado');
 const totalSpan = document.getElementById('total');
 const confirmarBtn = document.getElementById('confirmarBtn');
+
+cantidadInput.addEventListener('input', () => {
+    const cantidad = parseInt(cantidadInput.value);
+    if (cantidad >= 1 && cantidad <= 10) {
+      localidadSection.classList.remove('d-none');
+      segundaLocalidadSection.classList.remove('d-none');
+    } else {
+      localidadSection.classList.add('d-none');
+      segundaLocalidadSection.classList.add('d-none');
+      resultado.classList.add('d-none');
+    }
+  });
